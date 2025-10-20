7bce8f3c1243bfc06b40b68bae399351d0818033
Dominica Queendaine <qdomzy@gmail.com>
2025-10-21 00:36:50 +0100

    npm run build

--- Frontend/utils/generateQR.js
+++ Frontend/utils/generateQR.js
@@ -1,11 +1,8 @@
 // utils/generateQR.js
 import QRCode from "qrcode";
 
-export async function generateQR(url) {
-  try {
-    return await QRCode.toDataURL(url);
-  } catch (err) {
-    console.error("QR generation failed:", err);
-    return null;
-  }
+export async function generateQR(text) {
+  const qrApi = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${encodeURIComponent(text)}`;
+  return qrApi;
 }
+
