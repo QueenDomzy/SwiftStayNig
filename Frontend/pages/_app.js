fd033f0933011a374812867b064205e42dca34dd
Dominica Queendaine <qdomzy@gmail.com>
2025-10-21 02:11:10 +0100

    npm run build

--- Frontend/pages/_app.js
+++ Frontend/pages/_app.js
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 import { AuthProvider } from "../context/AuthContext";
-import SwiftBot from "@/components/SwiftBot";
+import SwiftBot from "../components/SwiftBot";
 
 export default function App({ Component, pageProps }) {
   return (
