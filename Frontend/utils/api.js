191918c3d3e98f64c21b3bddee3d09efcd51253c
Dominica Queendaine <qdomzy@gmail.com>
2025-10-24 14:20:09 +0100

    Update api.js

--- Frontend/utils/api.js
+++ Frontend/utils/api.js
@@ -1,7 +1,9 @@
+// frontend/utils/api.js
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_API_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://swiftstaynigeria-ua1e.onrender.com/api",
+  timeout: 10000,
 });
 
 export default api;
