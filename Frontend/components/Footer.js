bef946e9e08029389513fe33cb3342766bb13f76
Dominica Queendaine <qdomzy@gmail.com>
2025-10-25 15:34:02 +0100

    Update Footer.js

--- Frontend/components/Footer.js
+++ Frontend/components/Footer.js
@@ -1,3 +1,3 @@
 export default function Footer() {
-  return <footer className="bg-gray-800 text-white p-4 text-center">SwiftStay Footer</footer>;
+  return <footer className="bg-gray-800 text-white p-4 text-center">SwiftStaynig Footer</footer>;
 }
