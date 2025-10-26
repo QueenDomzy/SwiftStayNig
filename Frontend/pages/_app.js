cdd42771f4a13e1836558700fdef82d7e18d47a5
Dominica Queendaine <qdomzy@gmail.com>
2025-10-26 15:35:09 +0100

    Update _app.js

--- Frontend/pages/_app.js
+++ Frontend/pages/_app.js
@@ -2,11 +2,17 @@ import "../styles/globals.css";
 import { AuthProvider } from "../context/AuthContext";
 import SwiftBot from "../components/SwiftBot";
 
-export default function App({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
+      {/* Main page content */}
       <Component {...pageProps} />
+
+      {/* Global bot component */}
       <SwiftBot />
     </AuthProvider>
   );
 }
+
+export default MyApp;
+
